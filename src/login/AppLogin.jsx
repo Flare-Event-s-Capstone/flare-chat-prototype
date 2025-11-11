55178c735f85a645feaba05d5bfb617b948dd0a4
Emikcie_R <andyroman20@gmail.com>
2025-11-10 22:21:31 -0700

    Fix routing for GitHub Pages using HashRouter

--- src/login/AppLogin.jsx
+++ src/login/AppLogin.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./LoginPage";
 import ResetPassword from "./ResetPassword";
 import ResetSent from "./ResetSent";
