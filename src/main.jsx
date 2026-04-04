9b4f4a884946aacd1d15bbb8697d76b04db9973f
Emikcie_R <andyroman20@gmail.com>
2026-04-04 16:10:33 -0700

    Refactor routing in main.jsx for dashboard structure

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,31 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './login/LoginPage.jsx'
+import CreateUserPage from './login/CreateUserPage.jsx'
 import ResetPassword from './login/ResetPassword.jsx'
 import ResetSent from './login/ResetSent.jsx'
 import ResetPasswordLink from './login/ResetPasswordLink.jsx'
-import App from './App.jsx' 
-import ChatPage from "./components/ChatPage.jsx"
+import Dashboard from './dashboard/Dashboard.jsx'
+import EventsPanel from './dashboard/EventsPanel.jsx'
+import SettingsPanel from './dashboard/SettingsPanel.jsx'
+import MatchesPanel from './dashboard/MatchesPanel.jsx'
+import ChatPage from './components/ChatPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/flare-chat-prototype">
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/create-user" element={<CreateUserPage />} />
         <Route path="/reset" element={<ResetPassword />} />
         <Route path="/reset/sent" element={<ResetSent />} />
         <Route path="/reset/:resetToken" element={<ResetPasswordLink />} />
-        <Route path="/dashboard" element={<App />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/chat/:matchid" element={<ChatPage />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route index element={<MatchesPanel />} />
+          <Route path="settings" element={<SettingsPanel />} />
+          <Route path="events" element={<EventsPanel />} />
+          <Route path="chat/:matchid" element={<ChatPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
